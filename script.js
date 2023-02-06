8bd2f88ca920536d904a0e4ea36ebe79c90221e5
fadumasaidibrahim <fadumasaid2022@gmail.com>
2023-02-06 18:28:35 +0000

    added the api

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=50&lon=40&appid=5f81365ae536b7da813d034c891315db"
+const apiKey = "5f81365ae536b7da813d034c891315db";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
-fetch(apiUrl).then(function (response) {
-    return response.json()
+const searchBtn = document.getElementById("search-btn");
+const cityInput = document.getElementById("city-input");
+const weatherDisplay = document.getElementById("weather-display");
 
-}).then(function (data) {
-    console.log(data)
-})
+searchBtn.addEventListener("click", function() {
+  const city = cityInput.value;
 
+  fetch(`${weatherUrl}?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const lat = data.coord.lat;
+      const lon = data.coord.lon;
 
+      fetch(`${forecastUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+          let output = "";
+          data.list.forEach(function(forecast) {
+            output += `
+              <div>
+                <p>Date: ${forecast.dt_txt}</p>
+                <p>Temperature: ${forecast.main.temp}</p>
+                <p>Description: ${forecast.weather[0].description}</p>
+              </div>
+            `;
+          });
+          weatherDisplay.innerHTML = output;
+        });
+    });
+});
